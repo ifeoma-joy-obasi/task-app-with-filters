903afbab6c4ae0dcd62cdd97350e70044ea6fe56
ifeoma-joy-obasi <ifeomaobasi1999@gmail.com>
2023-11-25 09:23:41 +0100

    secound commit

---
+++ data.js
@@ -0,0 +1,57 @@
+let date = new Date().toLocaleDateString("en-us", { day: "numeric" });
+let month = new Date().toLocaleDateString("en-us", { month: "short" });
+let year = new Date().toLocaleDateString("en-us", { year: "numeric" });
+const FormattedDate = `${date}th ${month[0].toLowerCase() + month.slice(1)}, ${year}`;
+
+
+let data = [
+  {
+    id: Math.random(),
+    description: "do some shopping at maxland shopping mall",
+    status: "Done",
+    date: FormattedDate,
+    priority: "High",
+  },
+  {
+    id: Math.random(),
+    description: "fill my 12.5kg gas",
+    status: "inprogress",
+    date: FormattedDate,
+    priority: "Low",
+  },
+  {
+    id: Math.random(),
+    description: "hang out with friends for about 1hr",
+    status: "todo",
+    date: FormattedDate,
+    priority: "medium",
+  },
+  {
+    id: Math.random(),
+    description: "cut my hair",
+    status: "todo",
+    date: FormattedDate,
+    priority: "High",
+  },
+  {
+    id: Math.random(),
+    description: "refill my car fuel tank",
+    status: "inprogress",
+    date: FormattedDate,
+    priority: "High",
+  },
+  {
+    id: Math.random(),
+    description: "work on my project",
+    status: "inprogress",
+    date: FormattedDate,
+    priority: "Low",
+  },
+  {
+    id: Math.random(),
+    description: "spend some time with friends",
+    status: "Done",
+    date: FormattedDate,
+    priority: "Low",
+  },
+];
--- task-app.js
+++ task-app.js
@@ -1,6 +1,7 @@
 //variables
 let searchInput = document.querySelector("#searchInput");
 let closeIcon = document.querySelector("#closeIcon");
+let tableBody = document.querySelector("#tableBody");
 
 searchInput.addEventListener('focus', () => {
     closeIcon.style.display = 'block'
@@ -16,5 +17,103 @@ closeIcon.addEventListener('mousedown', (e) => {
     searchInput.value = ''
     
 })
+
+
+
+const upDateUi = () => {
+    tableHeadings.innerHTML = `
+      <th class="serial-num">S/N</th>
+            <th class="description">Description</th>
+            <th class="status">Status</th>
+            <th class="date">Date</th>
+            <th class="priority">priority</th>
+            <th class="icon">icon</th>
+      `;
+    data.map((task, index) => {
+      let tr = document.createElement("tr");
+      tr.setAttribute('class', 'border-bottom');
+      tr.innerHTML += `
+    <td>${index + 1}</td>
+    <td>${task.description}</td>
+    <td><button class = ${task.priority.toLowerCase()}>${task.status}</button></td>
+    <td>${task.date}</td>
+    <td><button class = ${task.status.toLowerCase()}>${task.priority}</button></td>
+    <td><img src=${"./images/vertical-dots.svg"} alt="three dot icon" class="vertical-icon"><td>
+    `;
+      console.log(tr)
+      tableBody.appendChild(tr);
+    });
+  }
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  upDateUi();
+})
+
+//search functionality
+
+let filteredList = [];
+
+let tableHeadingContainer = document.querySelector("#tableHeadingContainer");
+let tableHeadings = document.querySelector("#tableHeadings");
+
+const upDateFilteredUi = () => {
+  tableBody.innerHTML = "";
+  tableHeadings.innerHTML = "";
+  if (filteredList.length > 0) {
+    filteredList.map((task, index) => {
+      //i want to empty the empty state element when the array is being filtered
+      emptyState.innerHTML = "";
+      
+      //as the filteredList array is being filtered i want to show the thead element content
+      tableHeadings.innerHTML = `
+      <th class="serial-num">S/N</th>
+            <th class="description">Description</th>
+            <th class="status">Status</th>
+            <th class="date">Date</th>
+            <th class="priority">priority</th>
+            <th class="icon">icon</th>
+      `;
+
+      //create and append the tr element to the tbody element
+      let tr = document.createElement("tr");
+      tr.setAttribute("class", "border-bottom");
+      tr.innerHTML += `
+      <td>${index + 1}</td>
+      <td>${task.description}</td>
+      <td><button class = ${task.priority.toLowerCase()}>${
+        task.status
+      }</button></td>
+      <td>${task.date}</td>
+      <td><button class=${`${task.status.toLowerCase()}`}>${
+        task.priority
+      }</button></td>
+      <td><img src=${"./images/vertical-dots.svg"} alt="three dot icon" class="vertical-icon"><td>
+      `;
+      console.log(tr)
+      tableBody.appendChild(tr);
+    });
+  }
+  
+  if (filteredList.length <= 0) {
+    let emptyState = document.querySelector("#emptyState");
+    emptyState.innerHTML = `
+    <img src="./images/icon-search-1 (1).svg" alt="icon search for empty states" class="empty-state-search-icon">
+    <h4>No Result for "${searchInput.value}"</h4>
+    <span>check the spelling or try a new search</span>
+    `;
+  }
+};
+
+
+searchInput.addEventListener("input", (e) => {
+  let value = e.target.value.trim();
+
+  filteredList = data.filter((obj) => {
+      return obj.description.toLowerCase().includes(value.toLowerCase());
+  });
+   upDateFilteredUi();
+});
 
 
